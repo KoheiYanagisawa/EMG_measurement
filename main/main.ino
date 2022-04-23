45846fe2db44cf6f1b2a067a93cf950f3a683583
KoheiYanagisawa <yanahei0514@gmail.com>
2022-04-24 02:33:45 +0900

    first commit

---
+++ main/main.ino
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <MsTimer2.h>
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(A0,INPUT);
+  MsTimer2::set(1,flash); //1kHz
+  MsTimer2::start();
+}
+void flash(){
+  int sensorValue = analogRead(A0);
+  float voltage = sensorValue * (5.0/1023.0);
+
+  Serial.println(voltage);
+  }
+void loop() {
+  
+}
